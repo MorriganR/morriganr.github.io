e618844215e60da1c7c9c382b4f6db023e6101d3
MorriganR <dasha@ddyka.com>
2022-08-12 13:58:45 -0700

    fix padding

--- BuildReadme.py
+++ BuildReadme.py
@@ -7,10 +7,16 @@ mdFileName = './README.md'
 tableStyle = """
 <style>
 table {
-    border-collapse: collapse;
+  border-collapse: collapse;
 }
 table, th, td {
-   border: 1px solid black;
+  border: 1px solid black;
+  padding-left:5px;
+  padding-right:5px;
+}
+.main-content {
+  padding-left:13px;
+  padding-right:13px;
 }
 </style>
 """
@@ -25,9 +31,11 @@ indexHtml = open(indexHtmlFileName, 'a', encoding='utf-8')
 # set html charset
 indexHtml.write("""<meta http-equiv="Content-type" content="text/html; charset=utf-8" />""")
 indexHtml.write(tableStyle)
+indexHtml.write("""<div class="main-content">\n""")
 
 md = open(mdFileName, 'r', encoding='utf-8')
 indexHtml.write(markdown.markdown(md.read(), extensions=['tables', 'sane_lists'], output_format='html'))
 md.close()
 
+indexHtml.write("""\n</div>""")
 indexHtml.close()
